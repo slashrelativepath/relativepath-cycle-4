12203776fd991c322b91eddcdfbef3b6254acfba
KimberlyKulkovit <126214692+KimberlyKulkovit@users.noreply.github.com>
2023-06-21 15:52:04 -0700

    adding ps1 script

---
+++ myscript.ps1
@@ -0,0 +1,23 @@
+# powershell script to install chocolatey 
+
+echo "checking if chocolatey installed"
+if (get-command choco)
+{
+echo "chocholatey is already installed"
+}
+else
+{
+echo "installing chocolatey" 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+echo "checking if nano is installed"
+if (get-command nano)
+{
+echo "nano is already installed" 
+}
+else
+{
+echo "installing nano" 
+choco install nano
+}
