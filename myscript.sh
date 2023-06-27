2be322c4d767326b9f3decfd282c80571dc12a77
William McQuaid <whmcquaid@gmail.com>
2023-06-27 16:03:30 -0700

    Update bash script

--- myscript.sh
+++ myscript.sh
@@ -1,19 +1,33 @@
-x#!/bin/zsh
+#!/bin/bash
 
 # shell script to install homebrew
 
 echo "checking if brew is installed"
+
+# check operating system
 
 if (which brew)                   
 then 
   echo "brew is installed"
 else
   echo "installing brew"
-  /bin/bash -c "$(curl -fsSL 
-https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# checking if nano is installed
-# if nano installed
-# then echo "nano is already installed"
+echo "checking if nano is installed"
+if (which nano)
+then 
+echo "nano is already installed"
+else
+  echo "installing nano"
+  brew install nano
+fi
 
+echo "checking if git is installed"
+if (which git)
+then
+echo "git is already installed"
+else
+  echo "installing git"
+  brew install git
+fi
